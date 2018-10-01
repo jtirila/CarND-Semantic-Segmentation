6cc31fa2dd7cf578f5037c7424bd59e8df7050cc
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2018-10-01 15:41:32 +0300

    Revert skimage change

--- helper.py
+++ helper.py
@@ -1,6 +1,6 @@
 import re
 import random
-from skimage.transform import resize
+# from skimage.transform import resize
 import numpy as np
 import os.path
 import scipy.misc
@@ -85,11 +85,11 @@ def gen_batch_function(data_folder, image_shape):
             for image_file in image_paths[batch_i:batch_i+batch_size]:
                 gt_image_file = label_paths[os.path.basename(image_file)]
 
-                # image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
-                image = resize(scipy.misc.imread(image_file), image_shape)
+                image = scipy.misc.imresize(scipy.misc.imread(image_file), image_shape)
+                # image = resize(scipy.misc.imread(image_file), image_shape)
 
-                # gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
-                gt_image = resize(scipy.misc.imread(gt_image_file), image_shape)
+                gt_image = scipy.misc.imresize(scipy.misc.imread(gt_image_file), image_shape)
+                # gt_image = resize(scipy.misc.imread(gt_image_file), image_shape)
 
                 gt_bg = np.all(gt_image == background_color, axis=2)
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
