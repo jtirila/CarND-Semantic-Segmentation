2535f144e19ea3c67e96253cb04b5ee8d648bf9e
Juha-Matti Tirilä <juha-matti.tirila@op.fi>
2018-10-02 09:40:00 +0300

    Modify batch_size

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ tests.test_train_nn(train_nn)
 def run():
     num_classes = 2
     num_epochs = 36
-    batch_size = 64
+    batch_size = 32
     image_shape = (160, 576)
     data_dir = './data'
     runs_dir = './runs'
